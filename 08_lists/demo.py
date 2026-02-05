2a387700a53523d6536e32b334fa4c1a5bda1384
Sai chand <saarasaichand@gmail.com>
2026-02-05 18:44:00 +0530

    Delete 08_lists directory

--- 08_lists/demo.py
+++
@@ -1,6 +0,0 @@
-# simple variable
-value = 10 # regular int 
-valuse = 10,20,30 #data structure -> tuple
-
-print(type(value))
-print(type(valuse))
--- 08_lists/list.py
+++
@@ -1,2 +0,0 @@
-list_num = [10,20,30,40]
-print(list_num)
